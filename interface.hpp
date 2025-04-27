e8860e21cbeb5f95de253be25ac4d81e1c3d20e9
haris-afr <haris.afridi1312@gmail.com>
2025-04-27 14:42:41 +0500

    added files

---
+++ interface.hpp
@@ -0,0 +1,22 @@
+#ifndef INTERFACE_HPP
+#define INTEFACE_HPP
+class BaseMatrix{
+    int operator+(const BaseMatrix& rhs);
+    int operator-(const BaseMatrix& rhs);
+    int operator*(const BaseMatrix& rhs);
+    int Transpose();
+    bool Issingular();
+    int Determinant();
+    int RowEchelon();
+    int Rank();
+    int Display();
+    int ChangeMatrix();
+};
+class SquareMatrix: public BaseMatrix{
+    int Inverse();
+};
+class IdentityMatrix: public SquareMatrix{
+
+};
+
+#endif
