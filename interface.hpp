2b95d5a68373cf829d62f13a16e4790f12ac2882
Shoaib Hussain <shoaibhussainiqbal135@gmail.com>
2025-04-27 17:14:02 +0500

    Update interface.hpp

--- interface.hpp
+++ interface.hpp
@@ -6,6 +6,7 @@ class BaseMatrix{
     const int rows, columns;
     public:
     BaseMatrix(int x, int y);
+    ~BaseMatrix();
     BaseMatrix operator+(const BaseMatrix& rhs);
     BaseMatrix operator-(const BaseMatrix& rhs);
     BaseMatrix operator*(const BaseMatrix& rhs);
@@ -19,9 +20,9 @@ class SquareMatrix: public BaseMatrix{
     protected: 
     int size; //will be equal to either rows or columns
     public:
+    SquareMatrix(int a, int b);
     void Inverse(); // make the thing equal to matrix in function itself.
-    bool Issingular();
-    int Determinant();
+    double Determinant();
 };
 class IdentityMatrix: public SquareMatrix{
 
